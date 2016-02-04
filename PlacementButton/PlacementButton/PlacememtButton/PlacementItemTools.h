6dccff6d525b76b704d578c87ff06e4c163ea56c
Coody <coody0829@gmail.com>
2016-02-04 16:03:00 +0800

    1. 加入註解

--- PlacementButton/PlacementButton/PlacememtButton/PlacementItemTools.h
+++ PlacementButton/PlacementButton/PlacememtButton/PlacementItemTools.h
@@ -15,10 +15,20 @@
 
 #pragma mark - 建立動態按鈕
 
+/**
+ * @brief - 建立一個動態按鈕
+ * 
+ */
 -(void)createButtonWithPressedBlock:(void(^)(UIButton *responseButton))tempPressedButtonBlock;
 
+/**
+ * @brief - 隱藏/顯示所有動態按鈕
+ */
 -(void)setHidden:(BOOL)isHidden;
 
+/**
+ * @brief - 移除全部動態按鈕
+ */
 -(void)removeButtons;
 
 @end
