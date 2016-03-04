73d81ad4e73d09480d7c2578d940e3bc33eeca2a
Coody <coody0829@gmail.com>
2016-03-04 18:56:30 +0800

    1. 加入按鈕跟隨移動

--- PlacementButton/PlacementButton/PlacememtButton/PlacementItemTools.m
+++ PlacementButton/PlacementButton/PlacememtButton/PlacementItemTools.m
@@ -5,12 +5,30 @@
 //
 
 #import "PlacementItemTools.h"
-
 #import <UIKit/UIKit.h>
 
 // 會貼牆壁的範圍
 #define D_PlacementItemTools_Edge_TopY_Height (100)
 #define D_PlacementItemTools_Edge_BottomY_Height D_PlacementItemTools_Edge_TopY_Height
+/*
+ 說明：
+ 將畫面分成四個區塊：
+  ┌─────────────┐
+  │      1      │
+  ├──────┬──────┤
+  │      │      │
+  │      │      │
+  │  2   │   3  │
+  │      │      │
+  │      │      │
+  ├──────┴──────┤
+  │      4      │
+  └─────────────┘
+ 當按鈕中心點再這些範圍的時候，會自動修正回邊邊，
+ 如：當按鈕移動到 1 的時候，放開會回到 1 的螢幕邊框貼住；當按鈕移動到 3 的時候，方開會回到 3 的螢幕邊框（也就是右邊邊框）貼住。
+  D_PlacementItemTools_Edge_TopY_Height 就是設定 1 的高度
+  D_PlacementItemTools_Edge_BottomY_Height 就是設定 4 的高度
+ */
 
 // 離牆壁的微小距離
 #define D_PlacementItemTools_Margin (2)
@@ -26,9 +44,77 @@
 #pragma mark - Private UIButton
 @interface PlacementUIButton : UIButton
 @property (nonatomic , copy) void(^pressedButtonBlock)(UIButton *);
+-(void)followRootButton:(PlacementUIButton *)rootButton;
+-(void)endFollowRootButton:(PlacementUIButton *)rootButton;
+-(void)followCloselyWithButton:(PlacementUIButton *)rootButton;
+-(CGPoint)getEndCenterPoint:(CGPoint)targetCenterPoint;
 @end
 
 @implementation PlacementUIButton
+#pragma mark - 按鈕移動
+-(void)followRootButton:(PlacementUIButton *)rootButton{
+    CGFloat newX = self.center.x;
+    CGFloat newY = self.center.y;
+    CGFloat checkMaxX = rootButton.center.x + (D_PlacementItemTools_Width + 10);
+    CGFloat checkMinX = rootButton.center.x - (D_PlacementItemTools_Width + 10);
+    CGFloat checkMaxY = rootButton.center.y + (D_PlacementItemTools_Height + 10);
+    CGFloat checkMinY = rootButton.center.y - (D_PlacementItemTools_Height + 10);
+    if ( checkMaxX < newX ) {
+        newX = checkMaxX;
+    }
+    else if( checkMinX > newX ){
+        newX = checkMinX;
+    }
+    
+    if ( checkMaxY < newY ) {
+        newY = checkMaxY;
+    }
+    else if( checkMinY > newY ){
+        newY = checkMinY;
+    }
+    
+    self.center = CGPointMake(newX, newY);
+}
+
+-(void)endFollowRootButton:(PlacementUIButton *)rootButton{
+    __weak __typeof(self)weakSelf = self;
+    [UIView animateWithDuration:0.5f animations:^{
+        __strong __typeof(weakSelf)strongSelf = weakSelf;
+        strongSelf.center = [strongSelf getEndCenterPoint:rootButton.center];
+    }];
+}
+
+-(void)followCloselyWithButton:(PlacementUIButton *)rootButton{
+    
+}
+
+-(CGPoint)getEndCenterPoint:(CGPoint)targetCenterPoint{
+    CGFloat fixX = targetCenterPoint.x;
+    CGFloat fixY = targetCenterPoint.y;
+    CGPoint newCenterPoint;
+    CGSize mainScreenSize = CGSizeMake([UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height);
+    
+    if ( targetCenterPoint.y >= mainScreenSize.height - D_PlacementItemTools_Edge_BottomY_Height ||
+         targetCenterPoint.y <= D_PlacementItemTools_Edge_BottomY_Height ) {
+        // 位在區域 1,4
+        // 放在 TargetPoint 的右邊，如果超過，就自動改成左邊
+        fixX = targetCenterPoint.x + D_PlacementItemTools_Width + D_PlacementItemTools_Margin;
+        if ( fixX > mainScreenSize.width - D_PlacementItemTools_Width*0.5 - D_PlacementItemTools_Margin ) {
+            fixX = targetCenterPoint.x - D_PlacementItemTools_Width - D_PlacementItemTools_Width;
+        }
+    }
+    else{
+        // 位在區域 2,3
+        // 放在 TargetPoint 的下面，如果超過，就自動改成上面
+        fixY = targetCenterPoint.y + D_PlacementItemTools_Height + D_PlacementItemTools_Margin;
+        if ( fixY > mainScreenSize.height - D_PlacementItemTools_Width*0.5 - D_PlacementItemTools_Margin ) {
+            fixY = targetCenterPoint.y - D_PlacementItemTools_Height - D_PlacementItemTools_Margin;
+        }
+    }
+    newCenterPoint = CGPointMake(fixX, fixY);
+    return newCenterPoint;
+}
+
 @end
 
 
@@ -36,11 +122,10 @@
 @interface PlacementItemTools()
 {
     UIWindow *_tempKeyWindow;
-    CGSize _mainScreenSize;
 }
 @property (nonatomic , assign) NSInteger buttonIndex;
 @property (nonatomic , strong) NSMutableDictionary *buttonDic;
-
+@property (nonatomic , strong) NSArray *buttonArray;
 @end
 
 @implementation PlacementItemTools
@@ -91,19 +176,32 @@
         _buttonIndex = 0;
         _tempKeyWindow = [UIApplication sharedApplication].keyWindow;
         _buttonDic = [[NSMutableDictionary alloc] init];
-        _mainScreenSize = CGSizeMake([UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height);
     }
     return self;
 }
 
 -(void)createButtonWithPressedBlock:(void(^)(UIButton *responseButton))tempPressedButtonBlock
 {
+    if ( [_buttonArray count] >= 2 ) {
+        return;
+    }
     _buttonIndex = _buttonIndex + 1;
     
-    PlacementUIButton *button = [[PlacementUIButton alloc] initWithFrame:CGRectMake(D_PlacementItemTools_Init_X, 
-                                                                                    D_PlacementItemTools_Init_Y, 
-                                                                                    D_PlacementItemTools_Width, 
-                                                                                    D_PlacementItemTools_Width)];
+    PlacementUIButton *button;
+    if ( _buttonIndex == 0 ) {
+        button = [[PlacementUIButton alloc] initWithFrame:CGRectMake(D_PlacementItemTools_Init_X, 
+                                                                     D_PlacementItemTools_Init_Y, 
+                                                                     D_PlacementItemTools_Width, 
+                                                                     D_PlacementItemTools_Height)];
+    }
+    else{
+        UIButton *latestButton = [_buttonDic objectForKey:@(_buttonIndex - 1)];
+        button = [[PlacementUIButton alloc] initWithFrame:CGRectMake(latestButton.frame.origin.x, 
+                                                                     latestButton.frame.origin.y + latestButton.frame.size.height + 10 , 
+                                                                     D_PlacementItemTools_Width, 
+                                                                     D_PlacementItemTools_Height)];
+    }
+    
     [button setBackgroundColor:[UIColor lightGrayColor]];
     button.tag = _buttonIndex;
     button.pressedButtonBlock = tempPressedButtonBlock;
@@ -118,6 +216,17 @@
     [_tempKeyWindow addSubview:button];
     
     [_buttonDic setObject:button forKey:@(_buttonIndex)];
+    _buttonArray = [[_buttonDic allValues] sortedArrayUsingComparator:^NSComparisonResult( PlacementUIButton *obj1 , PlacementUIButton *obj2) {
+        if ( obj1.tag < obj2.tag ) {
+            return (NSComparisonResult)NSOrderedAscending;
+        }
+        else if( obj1.tag > obj2.tag){
+            return (NSComparisonResult)NSOrderedDescending;
+        }
+        else{
+            return (NSComparisonResult)NSOrderedSame;
+        }
+    }];
 }
 
 -(void)beginTouch:(UIButton *)button{
@@ -138,6 +247,37 @@
     // move button
     button.center = CGPointMake(button.center.x + delta_x,
                                 button.center.y + delta_y);
+    
+    // 移動其他的 point
+    /*
+     以一個框框為範圍縮限拉動距離， Dragged Exit 的時候再歸位就好
+     */
+    if ( [_buttonDic count] > 1 ) {
+        
+        /*
+         演算法： 1~5 ，有 5 個按鈕，假設 3 被當成拖曳對象，
+         則跟隨的順序會變成： 3 -> 1 -> 2 -> 4 -> 5
+         ( 也就是說，當「 4 號按鈕」取出來要跟隨「 3 號按鈕」的時候，發現「 3 號按鈕」跟「拖曳的按鈕」一樣，就改成跟「 2 號按鈕」。)
+         */
+        
+        NSUInteger index = 0;
+        
+        for ( PlacementUIButton *unit in _buttonArray ) {
+            if ( unit != button ) {
+                if ( index == 0  ) {
+                    [unit followRootButton:(PlacementUIButton *)button];
+                }
+                else{
+                    PlacementUIButton *checkButton = [_buttonArray objectAtIndex:(index - 1)];
+                    if ( checkButton == button  && index > 1 ) {
+                        checkButton = [_buttonArray objectAtIndex:(index - 2)];
+                    }
+                    [unit followRootButton:checkButton];
+                }
+            }
+            index++;
+        }
+    }
 }
 
 - (void)wasDraggedExit:(PlacementUIButton *)button
@@ -147,31 +287,8 @@
                                             button.center.y);
     CGPoint tempCheckTouchPoint = resultCenterPoint;
     
-    // Y 值在 0~80 以及 400~480 的範圍（已高度 480 來算）
-    if ( resultCenterPoint.y <= D_PlacementItemTools_Edge_TopY_Height) {
-        // 移動到最上面（ x 不動 , y 動 ）
-        resultCenterPoint.y = D_PlacementItemTools_Height*0.5 + D_PlacementItemTools_Margin;
-        
-        // 如果 x 超過邊界，要回到邊界
-        resultCenterPoint.x = [self checkX:resultCenterPoint.x];
-    }
-    else if ( resultCenterPoint.y >= _mainScreenSize.height - D_PlacementItemTools_Edge_BottomY_Height ){
-        // 移動到最下面（ x 不動 , y 動 ）
-        resultCenterPoint.y = _mainScreenSize.height - D_PlacementItemTools_Height*0.5 - D_PlacementItemTools_Margin;
-        
-        // 如果 x 超過邊界，要回到邊界
-        resultCenterPoint.x = [self checkX:resultCenterPoint.x];
-    }
-    else{
-        if ( resultCenterPoint.x > _mainScreenSize.width*0.5 ) {
-            // 移動到最右邊（ x 動 , y 不動 ）
-            resultCenterPoint.x = _mainScreenSize.width - D_PlacementItemTools_Width*0.5 - D_PlacementItemTools_Margin;
-        }
-        else{
-            // 移動到最左邊（ x 動 , y 不動 ）
-            resultCenterPoint.x = D_PlacementItemTools_Width*0.5 + D_PlacementItemTools_Margin;
-        }
-    }
+    // 將中心點丟進類別方法，來調整其位置
+    resultCenterPoint = [PlacementItemTools getFixCenterPoint:resultCenterPoint];
     
     [UIView animateWithDuration:0.25f animations:^{
         // move button
@@ -185,19 +302,83 @@
             }
         }
     }];
+    
+    // 移動其他的 point
+    /*
+     以一個框框為範圍縮限拉動距離， Dragged Exit 的時候再歸位就好
+     */
+    if ( [_buttonDic count] > 1 ) {
+        
+        /*
+         演算法： 1~5 ，有 5 個按鈕，假設 3 被當成拖曳對象，
+         則跟隨的順序會變成： 3 -> 1 -> 2 -> 4 -> 5
+         ( 也就是說，當「 4 號按鈕」取出來要跟隨「 3 號按鈕」的時候，發現「 3 號按鈕」跟「拖曳的按鈕」一樣，就改成跟「 2 號按鈕」。)
+         */
+        
+        NSUInteger index = 0;
+        
+        for ( PlacementUIButton *unit in _buttonArray ) {
+            if ( unit != button ) {
+                if ( index == 0  ) {
+                    [unit endFollowRootButton:(PlacementUIButton *)button];
+                }
+                else{
+                    PlacementUIButton *checkButton = [_buttonArray objectAtIndex:(index - 1)];
+                    if ( checkButton == button  && index > 1 ) {
+                        checkButton = [_buttonArray objectAtIndex:(index - 2)];
+                    }
+                    [unit endFollowRootButton:checkButton];
+                }
+            }
+            index++;
+        }
+    }
 }
 
--(CGFloat)checkX:(CGFloat)recentX{
++(CGFloat)checkX:(CGFloat)recentX{
     // 如果 x 超過邊界，要回到邊界
+    CGSize mainScreenSize = CGSizeMake([UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height);
     CGFloat newX = recentX;
     if ( recentX < D_PlacementItemTools_Width*0.5 + D_PlacementItemTools_Margin ) {
         newX = D_PlacementItemTools_Width*0.5 + D_PlacementItemTools_Margin;
     }
-    else if( recentX > _mainScreenSize.width - D_PlacementItemTools_Width*0.5 - D_PlacementItemTools_Margin ){
-        newX = _mainScreenSize.width - D_PlacementItemTools_Width*0.5 - D_PlacementItemTools_Margin;
+    else if( recentX > mainScreenSize.width - D_PlacementItemTools_Width*0.5 - D_PlacementItemTools_Margin ){
+        newX = mainScreenSize.width - D_PlacementItemTools_Width*0.5 - D_PlacementItemTools_Margin;
     }
     return newX;
 }
 
++(CGPoint)getFixCenterPoint:(CGPoint)originalPoint{
+    CGPoint fixPoint = originalPoint;
+    CGSize mainScreenSize = CGSizeMake([UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height);
+    
+    // Y 值在 0~80 以及 400~480 的範圍（已高度 480 來算）
+    if ( fixPoint.y <= D_PlacementItemTools_Edge_TopY_Height) {
+        // 移動到最上面（ x 不動 , y 動 ）
+        fixPoint.y = D_PlacementItemTools_Height*0.5 + D_PlacementItemTools_Margin;
+        
+        // 如果 x 超過邊界，要回到邊界
+        fixPoint.x = [PlacementItemTools checkX:fixPoint.x];
+    }
+    else if ( fixPoint.y >= mainScreenSize.height - D_PlacementItemTools_Edge_BottomY_Height ){
+        // 移動到最下面（ x 不動 , y 動 ）
+        fixPoint.y = mainScreenSize.height - D_PlacementItemTools_Height*0.5 - D_PlacementItemTools_Margin;
+        
+        // 如果 x 超過邊界，要回到邊界
+        fixPoint.x = [self checkX:originalPoint.x];
+    }
+    else{
+        if ( fixPoint.x > mainScreenSize.width*0.5 ) {
+            // 移動到最右邊（ x 動 , y 不動 ）
+            fixPoint.x = mainScreenSize.width - D_PlacementItemTools_Width*0.5 - D_PlacementItemTools_Margin;
+        }
+        else{
+            // 移動到最左邊（ x 動 , y 不動 ）
+            fixPoint.x = D_PlacementItemTools_Width*0.5 + D_PlacementItemTools_Margin;
+        }
+    }
+    
+    return fixPoint;
+}
 
 @end
