55cd9cae51b5b6738311bd1ec1f79c3825457341
Coody <coody0829@gmail.com>
2016-04-01 11:24:52 +0800

    1. 修正置頂為 0 的時候，Y軸位置不會退回螢幕畫面內 2. 修正案下後，縮起來的動畫效果（改成內縮跟隨）

--- PlacementButton/PlacementButton/PlacememtButton/PlacementItemTools.m
+++ PlacementButton/PlacementButton/PlacememtButton/PlacementItemTools.m
@@ -8,7 +8,7 @@
 #import <UIKit/UIKit.h>
 
 // 會貼牆壁的範圍
-#define D_PlacementItemTools_Edge_TopY_Height (100)
+#define D_PlacementItemTools_Edge_TopY_Height (0)
 #define D_PlacementItemTools_Edge_BottomY_Height D_PlacementItemTools_Edge_TopY_Height
 /*
  說明：
@@ -37,6 +37,9 @@
 #define D_PlacementItemTools_Width (50)
 #define D_PlacementItemTools_Height D_PlacementItemTools_Width
 
+// 按鈕透明度
+#define D_PlacementButton_Alpha (0.6f)
+
 // 按鈕起始位置
 #define D_PlacementItemTools_Init_X ([UIScreen mainScreen].bounds.size.width - D_PlacementItemTools_Width - D_PlacementItemTools_Margin)
 #define D_PlacementItemTools_Init_Y (D_PlacementItemTools_Height*2)
@@ -46,12 +49,15 @@ NSUInteger const kPlacementItemTools_Max_Button_Count = 2;
 
 // 動畫時間
 /** 目前拖拉按鈕放開後，回到邊緣的速度 */
-static CGFloat const kAnimationDuration_Move = 0.25f;
+static CGFloat const kAnimationDuration_Move = 0.15f;
 /** 跟隨的按鈕在拖拉按鈕放開後，回到邊緣的速度（建議慢一點點，會有跟隨移動的感覺） */
-static CGFloat const kAnimationDuration_Follow = 0.35f;
+static CGFloat const kAnimationDuration_Follow = 0.25f;
 
 #pragma mark - Private UIButton
 @interface PlacementUIButton : UIButton
+/** 是否是按鈕按下的狀態？ */
+@property (nonatomic , assign) BOOL isOnTouch;
+/** 是否 */
 @property (nonatomic , copy) void(^pressedButtonBlock)(UIButton *);
 -(void)followRootButton:(PlacementUIButton *)rootButton;
 -(void)endFollowRootButton:(PlacementUIButton *)rootButton;
@@ -60,14 +66,60 @@ static CGFloat const kAnimationDuration_Follow = 0.35f;
 @end
 
 @implementation PlacementUIButton
-#pragma mark - 按鈕移動
+-(instancetype)init{
+    self = [super init];
+    if ( self ) {
+        _isOnTouch = NO;
+    }
+    return self;
+}
+
+#pragma mark - 按鈕動畫
+-(void)beginMoveToRootButton:(PlacementUIButton *)rootButton{
+    CGFloat newX = self.center.x;
+    CGFloat newY = self.center.y;
+    CGFloat moveDistance = 12;
+    //    CGFloat checkMaxX = rootButton.center.x + (D_PlacementItemTools_Width + moveDistance);
+    //    CGFloat checkMinX = rootButton.center.x - (D_PlacementItemTools_Width + moveDistance);
+    //    CGFloat checkMaxY = rootButton.center.y + (D_PlacementItemTools_Height + moveDistance);
+    //    CGFloat checkMinY = rootButton.center.y - (D_PlacementItemTools_Height + moveDistance);
+    CGFloat checkMaxX = rootButton.center.x + moveDistance;
+    CGFloat checkMinX = rootButton.center.x - moveDistance;
+    CGFloat checkMaxY = rootButton.center.y + moveDistance;
+    CGFloat checkMinY = rootButton.center.y - moveDistance;
+    if ( checkMaxX < newX ) {
+        newX = checkMaxX;
+    }
+    else if( checkMinX > newX ){
+        newX = checkMinX;
+    }
+    
+    if ( checkMaxY < newY ) {
+        newY = checkMaxY;
+    }
+    else if( checkMinY > newY ){
+        newY = checkMinY;
+    }
+    
+    __weak __typeof(self)weakSelf = self;
+    [UIView animateWithDuration:kAnimationDuration_Move animations:^{
+        __strong __typeof(weakSelf)strongSelf = weakSelf;
+        strongSelf.center = CGPointMake(newX, newY);
+    }];
+}
+
 -(void)followRootButton:(PlacementUIButton *)rootButton{
     CGFloat newX = self.center.x;
     CGFloat newY = self.center.y;
-    CGFloat checkMaxX = rootButton.center.x + (D_PlacementItemTools_Width + 10);
-    CGFloat checkMinX = rootButton.center.x - (D_PlacementItemTools_Width + 10);
-    CGFloat checkMaxY = rootButton.center.y + (D_PlacementItemTools_Height + 10);
-    CGFloat checkMinY = rootButton.center.y - (D_PlacementItemTools_Height + 10);
+    CGFloat moveDistance = 12;
+//    CGFloat checkMaxX = rootButton.center.x + (D_PlacementItemTools_Width + moveDistance);
+//    CGFloat checkMinX = rootButton.center.x - (D_PlacementItemTools_Width + moveDistance);
+//    CGFloat checkMaxY = rootButton.center.y + (D_PlacementItemTools_Height + moveDistance);
+//    CGFloat checkMinY = rootButton.center.y - (D_PlacementItemTools_Height + moveDistance);
+    CGFloat checkMaxX = rootButton.center.x + moveDistance;
+    CGFloat checkMinX = rootButton.center.x - moveDistance;
+    CGFloat checkMaxY = rootButton.center.y + moveDistance;
+    CGFloat checkMinY = rootButton.center.y - moveDistance;
     if ( checkMaxX < newX ) {
         newX = checkMaxX;
     }
@@ -96,10 +148,10 @@ static CGFloat const kAnimationDuration_Follow = 0.35f;
 -(void)followCloselyWithButton:(PlacementUIButton *)rootButton{
     CGFloat newX = self.center.x;
     CGFloat newY = self.center.y;
-    CGFloat checkMaxX = rootButton.center.x + (D_PlacementItemTools_Width + D_PlacementItemTools_Margin);
-    CGFloat checkMinX = rootButton.center.x - (D_PlacementItemTools_Width + D_PlacementItemTools_Margin);
-    CGFloat checkMaxY = rootButton.center.y + (D_PlacementItemTools_Height + D_PlacementItemTools_Margin);
-    CGFloat checkMinY = rootButton.center.y - (D_PlacementItemTools_Height + D_PlacementItemTools_Margin);
+    CGFloat checkMaxX = rootButton.center.x + (D_PlacementItemTools_Margin);
+    CGFloat checkMinX = rootButton.center.x - (D_PlacementItemTools_Margin);
+    CGFloat checkMaxY = rootButton.center.y + (D_PlacementItemTools_Margin);
+    CGFloat checkMinY = rootButton.center.y - (D_PlacementItemTools_Margin);
     if ( checkMaxX < newX ) {
         newX = checkMaxX;
     }
@@ -152,6 +204,7 @@ static CGFloat const kAnimationDuration_Follow = 0.35f;
 {
     UIWindow *_tempKeyWindow;
 }
+@property (nonatomic , assign) BOOL isActive;
 @property (nonatomic , assign) NSInteger buttonIndex;
 @property (nonatomic , strong) NSMutableDictionary *buttonDic;
 @property (nonatomic , strong) NSArray *buttonArray;
@@ -166,6 +219,7 @@ static CGFloat const kAnimationDuration_Follow = 0.35f;
     dispatch_once(&pred, ^{
         if ( sharedInstance == nil ) {
             sharedInstance = [[self alloc] init];
+            sharedInstance.isActive = NO;
         }
     });
     return sharedInstance;
@@ -217,6 +271,14 @@ static CGFloat const kAnimationDuration_Follow = 0.35f;
     return self;
 }
 
+-(void)dealloc{
+    for ( UIButton *unit in [_buttonDic allValues] ) {
+        [unit removeFromSuperview];
+    }
+    [_buttonDic removeAllObjects];
+    _buttonDic = nil;
+}
+
 -(void)createButtonWithNormalImage:(UIImage *)normalImage 
                withHightLightImage:(UIImage *)hightLightImage 
                   WithPressedBlock:(void(^)(UIButton *responseButton))tempPressedButtonBlock
@@ -263,7 +325,7 @@ static CGFloat const kAnimationDuration_Follow = 0.35f;
 #endif
     button.layer.cornerRadius = 10.0f;
     button.layer.masksToBounds = YES;
-    [button setAlpha:0.6f];
+    [button setAlpha:D_PlacementButton_Alpha];
     [button addTarget:self action:@selector(beginTouch:) forControlEvents:(UIControlEventTouchDown)];
     [button addTarget:self action:@selector(wasDragged:withEvent:) forControlEvents:(UIControlEventTouchDragInside)];
     [button addTarget:self action:@selector(wasDraggedExit:) forControlEvents:(UIControlEventTouchUpInside)];
@@ -287,6 +349,49 @@ static CGFloat const kAnimationDuration_Follow = 0.35f;
 
 -(void)beginTouch:(UIButton *)button{
     [button setAlpha:1.0f];
+    
+    // 移動其他的 point
+    /*
+     以一個框框為範圍縮限拉動距離， Dragged Exit 的時候再歸位就好
+     */
+    if ( [_buttonDic count] > 1 ) {
+        
+        /*
+         演算法： 1~5 ，有 5 個按鈕，假設 3 被當成拖曳對象，
+         則跟隨的順序會變成： 3 -> 1 -> 2 -> 4 -> 5
+         ( 也就是說，當「 4 號按鈕」取出來要跟隨「 3 號按鈕」的時候，發現「 3 號按鈕」跟「拖曳的按鈕」一樣，就改成跟「 2 號按鈕」。)
+         */
+        
+        NSUInteger index = 0;
+        
+        for ( PlacementUIButton *unit in _buttonArray ) {
+            if ( unit != button ) {
+                if ( index == 0  ) {
+                    [unit beginMoveToRootButton:(PlacementUIButton *)button];
+                }
+                else{
+                    PlacementUIButton *checkButton = [_buttonArray objectAtIndex:(index - 1)];
+                    if ( checkButton == button ) {
+                        
+                        if ( index >= 2 ) {
+                            checkButton = [_buttonArray objectAtIndex:(index - 2)];
+                            [unit beginMoveToRootButton:checkButton];
+                        }
+                        else{
+                            [unit beginMoveToRootButton:checkButton];
+                        }
+                        
+                        
+                    }
+                    else{
+                        [unit beginMoveToRootButton:checkButton];
+                    }
+                }
+            }
+            index++;
+        }
+    }
+    
 }
 
 - (void)wasDragged:(UIButton *)button withEvent:(UIEvent *)event
@@ -303,6 +408,7 @@ static CGFloat const kAnimationDuration_Follow = 0.35f;
     // move button
     button.center = CGPointMake(button.center.x + delta_x,
                                 button.center.y + delta_y);
+    NSLog(@"( %.1f , %.1f )" , button.center.x , button.center.y );
     
     // 移動其他的 point
     /*
@@ -349,7 +455,6 @@ static CGFloat const kAnimationDuration_Follow = 0.35f;
 
 - (void)wasDraggedExit:(PlacementUIButton *)button
 {
-    
     CGPoint resultCenterPoint = CGPointMake(button.center.x,
                                             button.center.y);
     CGPoint tempCheckTouchPoint = resultCenterPoint;
@@ -357,13 +462,44 @@ static CGFloat const kAnimationDuration_Follow = 0.35f;
     // 將中心點丟進類別方法，來調整其位置
     resultCenterPoint = [PlacementItemTools getFixCenterPoint:resultCenterPoint];
     
+    __weak __typeof(self) weakSelf = self;
     [UIView animateWithDuration:kAnimationDuration_Move animations:^{
         // move button
         button.center = resultCenterPoint;
-        [button setAlpha:0.6f];
+        [button setAlpha:D_PlacementButton_Alpha];
+        
     } completion:^(BOOL finished) {
         if ( finished ) {
             if ( tempCheckTouchPoint.x == button.center.x && tempCheckTouchPoint.y == button.center.y ) {
+                // 看狀況移動到上方
+                __strong __typeof(weakSelf) strongSelf = weakSelf;
+                if ( strongSelf.isActive == NO ) {
+                    /* 沒有任何按鈕被按到，所以所有按鈕移動到上方 */
+                    strongSelf.isActive = YES;
+                    button.isOnTouch = YES;
+                    
+                }
+                else{
+                    /* 
+                     有兩種可能：
+                     1. 同一個按鈕被按到，所以要結束移動到上方，所有按鈕回到原位
+                     2. 另一個按鈕被按到，所以要開啟另一個按鈕的狀態，然後內部其中有個按鈕要回復沒被按到的狀態
+                     */
+                    if ( button.isOnTouch == YES ) {
+                        strongSelf.isActive = NO;
+                        button.isOnTouch = NO;
+                    }
+                    else{
+                        for (PlacementUIButton *unit in strongSelf.buttonArray) {
+                            if ( unit.isOnTouch == YES ) {
+                                unit.isOnTouch = NO;
+                                break;
+                            }
+                        }
+                        button.isOnTouch = YES;
+                    }
+                }
+                
                 // Touch
                 button.pressedButtonBlock(button);
             }
@@ -404,7 +540,7 @@ static CGFloat const kAnimationDuration_Follow = 0.35f;
 
 +(CGFloat)checkX:(CGFloat)recentX{
     // 如果 x 超過邊界，要回到邊界
-    CGSize mainScreenSize = CGSizeMake([UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height);
+    CGSize mainScreenSize = [UIScreen mainScreen].bounds.size;
     CGFloat newX = recentX;
     if ( recentX < D_PlacementItemTools_Width*0.5 + D_PlacementItemTools_Margin ) {
         newX = D_PlacementItemTools_Width*0.5 + D_PlacementItemTools_Margin;
@@ -413,6 +549,19 @@ static CGFloat const kAnimationDuration_Follow = 0.35f;
         newX = mainScreenSize.width - D_PlacementItemTools_Width*0.5 - D_PlacementItemTools_Margin;
     }
     return newX;
+}
+
++(CGFloat)checkY:(CGFloat)recentY{
+    // 如果 x 超過邊界，要回到邊界
+    CGSize mainScreenSize = [UIScreen mainScreen].bounds.size;
+    CGFloat newY = recentY;
+    if ( recentY < D_PlacementItemTools_Height*0.5 + D_PlacementItemTools_Margin + D_PlacementItemTools_Margin*5 ) {
+        newY = D_PlacementItemTools_Height*0.5 + D_PlacementItemTools_Margin + D_PlacementItemTools_Margin*5;
+    }
+    else if( recentY > mainScreenSize.height - D_PlacementItemTools_Height*0.5 - D_PlacementItemTools_Margin ){
+        newY = mainScreenSize.height - D_PlacementItemTools_Height*0.5 - D_PlacementItemTools_Margin;
+    }
+    return newY;
 }
 
 +(CGPoint)getFixCenterPoint:(CGPoint)originalPoint{
@@ -432,7 +581,7 @@ static CGFloat const kAnimationDuration_Follow = 0.35f;
         fixPoint.y = mainScreenSize.height - D_PlacementItemTools_Height*0.5 - D_PlacementItemTools_Margin;
         
         // 如果 x 超過邊界，要回到邊界
-        fixPoint.x = [self checkX:originalPoint.x];
+        fixPoint.x = [PlacementItemTools checkX:originalPoint.x];
     }
     else{
         if ( fixPoint.x > mainScreenSize.width*0.5 ) {
@@ -443,6 +592,9 @@ static CGFloat const kAnimationDuration_Follow = 0.35f;
             // 移動到最左邊（ x 動 , y 不動 ）
             fixPoint.x = D_PlacementItemTools_Width*0.5 + D_PlacementItemTools_Margin;
         }
+        
+        fixPoint.y = [PlacementItemTools checkY:fixPoint.y];
+        
     }
     
     return fixPoint;
